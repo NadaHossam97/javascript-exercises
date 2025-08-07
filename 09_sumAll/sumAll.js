6c295abde6331f794c639e6158393b6a49f96eab
JoeHossam <86380699+JoeHossam@users.noreply.github.com>
2025-08-07 23:30:27 +0300

    resuming solving

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    // Validations
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    } else if (typeof a != 'number' || typeof b != 'number') {
+        return 'ERROR';
+    } else if (a % 1 !== 0 || b % 1 !== 0) {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+    const [initial, last] = a < b ? [a, b] : [b, a];
+    // let sum = 0;
+    // let initial = 0;
+    // let last = 0;
+    // if (a < b) {
+    //     initial = a;
+    //     last = b;
+    // } else if (a > b) {
+    //     initial = b;
+    //     last = a;
+    // }
+
+    for (let i = initial; i < last + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
