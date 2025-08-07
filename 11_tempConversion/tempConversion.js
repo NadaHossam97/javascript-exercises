46b12ca34b44b24c91fe53316f018c8f3701477b
JoeHossam <86380699+JoeHossam@users.noreply.github.com>
2025-08-08 00:44:01 +0300

    resuming solving

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempF) {
+    const celsius = (tempF - 32) * (5 / 9);
+    let roundedC = celsius.toFixed(1);
+    if (roundedC % 1 == 0) {
+        return Math.round(roundedC);
+    } else {
+        return +roundedC;
+    }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+    const fahrenheit = tempC * (9 / 5) + 32;
+    let roundedF = fahrenheit.toFixed(1);
+    if (roundedF % 1 == 0) {
+        return Math.round(roundedF);
+    } else {
+        return +roundedF;
+    }
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit,
 };
