c65569730535fe2f7cf32b398011794cb2634dc9
NadaHossam97 <nadahossam8@yahoo.fr>
2025-08-12 18:00:05 +0300

    solving exercises

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+cleanedString = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
 
+reversedString = string.split('').reverse().join('');
+
+cleanedRversedString = reversedString.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+
+return cleanedString == cleanedRversedString
 };
 
 // Do not edit below this line
